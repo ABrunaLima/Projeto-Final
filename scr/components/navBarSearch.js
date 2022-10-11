ae3967d3d20eeed5ca3e46e0ef7ff2aef0d74a85
Bruna <brunaljesus94@gmail.com>
2022-10-11 10:24:23 +0100

    search

---
+++ scr/components/navBarSearch.js
@@ -0,0 +1,59 @@
+import styles from '/styles/navbar.module.css'
+import { Login } from './login'
+import { useRouter } from 'next/router'
+import closet from '../../pages/closet'
+
+
+export function NavBar() {
+    const navItems = [
+        {
+            label: "Home",
+            path: "/"
+        },
+        {
+            label: "Closet",
+            path: "/closet"                 //editado, testes de caminho        Daniel
+        }
+    ]
+
+
+    return (
+        <>
+            <nav className={styles.nav}>
+                <div className={styles.marca}>
+                    <img src="/images/LogoPreto.png" />
+                </div>
+                <div>
+                    //coloca aqui a search bar
+                </div>
+                <div className={styles.textos}>
+                    {
+                        navItems.map(n => <NavItem {...n} />)
+                    }
+                    <Login />
+                </div>
+            </nav>
+        </>
+    )
+}
+
+function NavItem({ path, label }) {
+
+    const router = useRouter()
+
+    const handleClick = (e) => {
+
+        e.preventDefault()
+        router.push(path)
+
+    }
+
+    return (
+
+        //o href, busca o caminho da página que é selecionado com o onClick quando se carrega numa das labels(botões) 
+        <span href={path} onClick={handleClick}>
+            {label}
+        </span>
+
+    )
+}
