c54b32f3a5388b0c50f89eb6a1d261711fd1dacd
Bruna <brunaljesus94@gmail.com>
2022-10-12 11:14:25 +0100

    index

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/navbar.module.css'
-import styles2 from '../styles/index.module.css'
 import { NavBar } from '../scr/components/navbar'
-import { closet } from './closet'
-import { NavBarSearch } from '../scr/components/navBarSearch'
+import styles from '../styles/initialPage.module.css'
 
 
 export default function Home() {
   return (
-
-    <div>
-
-      <div>
-
-        <NavBarSearch />
-
+    <div className={styles.container}>
+      <NavBar />
+      <div className={styles.texto}>
+        <h1>Make your looks and keep it on your virtual closets</h1>
       </div>
-
-      <div className={styles2.fundo}>
-
-        <Head>
-          <title>MyVest</title>
-
-          {/*tentando alterar o icon da pagina*/}
-
-          <link rel="icon" href="avicon_io/vest.ico" />
-
-          {/*<link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon_io/site.webmanifest" /> */}
-
-        </Head>
-
-        <closet />
-
+      <div className={styles.containerBotao}>
+        <button className={styles.botao}>
+          Start
+        </button>
       </div>
-
     </div>
-
   )
 }
