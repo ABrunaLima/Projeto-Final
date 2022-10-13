a161a383954e0f78ed5450f5c70c8b92aa5f53af
luis <luis98lffc@gmail.com>
2022-10-13 14:38:00 +0100

    lala

--- scr/components/filtros.js
+++ scr/components/filtros.js
@@ -11,49 +11,52 @@ export function Filtro({ data, setData }) {
                 return curDate.type === catItem
             })
         }
-            setData(result)
-        }
-
-        const corFiltrada = (catItem) => {
-            let result = Roupas
-            if (catItem !== "all") {
-                result = Roupas.filter((curDate) => {
-                    return curDate.color === catItem
-                })
-            }
-            setData(result)
-        }
-
-        return (
-            <div>
-                <div className={styles.titulos}>
-                    <p>Category</p>
-                </div>
-                <div className={styles.categorias}>
-                    <button onClick={() => categoriaFiltrada("all")}>All</button>
-                    <button onClick={() => categoriaFiltrada('blazer')}>Blazers</button>
-                    <button onClick={() => categoriaFiltrada('sweat')}>Sweats</button>
-                    <button onClick={() => categoriaFiltrada('coat')}>Coats</button>
-                    <button onClick={() => categoriaFiltrada('pants')}>Pants</button>
-                    <button onClick={() => categoriaFiltrada('skirt')}>Skirts</button>
-                    <button onClick={() => categoriaFiltrada('shoes')}>Shoes</button>
-                </div>
-                <div className={styles.titulos}>
-                    <p>Color</p>
-                </div>
-                <div className={styles.cores}>
-                    <button onClick={() => corFiltrada(data)}>All</button>
-                    <button onClick={() => corFiltrada('beige')}>Beige</button>
-                    <button onClick={() => corFiltrada('black')}>Black</button>
-                    <button onClick={() => corFiltrada('blue')}>Blue</button>
-                    <button onClick={() => corFiltrada('green')}>Green</button>
-                    <button onClick={() => corFiltrada('grey')}>Grey</button>
-                    <button onClick={() => corFiltrada('pink')}>Pink</button>
-                    <button onClick={() => corFiltrada('purple')}>Purple</button>
-                    <button onClick={() => corFiltrada('red')}>Red</button>
-                    <button onClick={() => corFiltrada('white')}>White</button>
-                </div>
-
-            </div>
-        );
+        setData(result)
     }
+
+    const corFiltrada = (catItem) => {
+        let result = Roupas
+        if (catItem !== "all") {
+            result = Roupas.filter((curDate) => {
+                return curDate.color === catItem
+            })
+        }
+        setData(result)
+    }
+
+    return (
+        <div className={styles.fora}>
+            <div className={styles.titulos}>
+                <p>Category</p>
+            </div>
+            <div className={styles.categorias}>
+                <button onClick={() => categoriaFiltrada("all")}>All</button>
+                <button onClick={() => categoriaFiltrada('blazer')}>Blazers</button>
+                <button onClick={() => categoriaFiltrada('sweat')}>Sweats</button>
+                <button onClick={() => categoriaFiltrada('coat')}>Coats</button>
+                <button onClick={() => categoriaFiltrada('pants')}>Pants</button>
+                <button onClick={() => categoriaFiltrada('skirt')}>Skirts</button>
+                <button onClick={() => categoriaFiltrada('shoes')}>Shoes</button>
+            </div>
+            <div className={styles.tituloscores}>
+                <p>Color</p>
+            </div>
+
+            <div className={styles.cores}>
+                <div className={styles.divcores}>
+                    <button onClick={() => corFiltrada(data)}>All</button>
+                    <button onClick={() => corFiltrada('beige')}><img src="/cores/beige.jpeg" /></button>
+                    <button onClick={() => corFiltrada('black')}><img src="/cores/black.jpeg" /></button>
+                    <button onClick={() => corFiltrada('blue')}><img src="/cores/blue.png" /></button>
+                    <button onClick={() => corFiltrada('green')}><img src="/cores/green.png" /></button>
+                    <button onClick={() => corFiltrada('grey')}><img src="/cores/grey.jpeg" /></button>
+                    <button onClick={() => corFiltrada('pink')}><img src="/cores/pink.png" /></button>
+                    <button onClick={() => corFiltrada('purple')}><img src="/cores/purple.png" /></button>
+                    <button onClick={() => corFiltrada('red')}><img src="/cores/red.png" /></button>
+                    <button onClick={() => corFiltrada('white')}><img src="/cores/white.png" /></button>
+                </div>
+            </div>
+
+        </div>
+    );
+}
