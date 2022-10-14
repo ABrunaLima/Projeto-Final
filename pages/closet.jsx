ce00ff9a59b5f5e7055de592df9c61832ed454f4
japananinha <hebicamera@gmail.com>
2022-10-14 13:57:09 +0100

    commit

--- pages/closet.jsx
+++ pages/closet.jsx
@@ -54,10 +54,30 @@ const closets = [
 
 ]
 
+
 export default function closet() {
 
     const router = useRouter()
 
+    function novoClosetVazio() {
+
+        const res = await fetch("/api/closet", {
+            method: "POST",
+            headers: {
+                "authorization": localStorage.getItem("token")
+            }
+        })
+
+        if (res.status === 200) {
+            const res = await fetch("/api/closet", {
+                method: "GET",
+                headers: {
+                    "authorization": localStorage.getItem("token")
+                }
+            })
+        }
+
+    }
 
     return (
         //**************************************************************************************************************
@@ -72,23 +92,6 @@ export default function closet() {
                         <div className={styles.title}>
                             <h2>Closets</h2>
                         </div>
-
-                        {/* <CDropdown /> */}
-
-                        {/* ------------------------------------------------------------------------------------------------ */}
-
-                        {/* <section for="botao-escolher-closet">
-                    <div className={dropDownButton.dropdown}>
-                        <button onClick={dropDownButton} className={styles2.dropbtn}>Escolher Closet</button>
-                        <div id="myDropdown" className={styles2.dropdownContent}>
-                            <a href="#closet1">Closet 1</a>
-                            <a href="#closet2">Closet 2</a>
-                            <a href="#closet3">Closet 3</a>
-                        </div>
-                    </div>
-                </section> */}
-
-                        {/* ------------------------------------------------------------------------------------------------ */}
 
                         <section>
 
@@ -107,64 +110,13 @@ export default function closet() {
                                     </div>
                                 ))}
 
-                                {/* <div className={styles.area}>
-
-                                    <div className={styles.imagem}>
-                                        <img src="camisaDoFlamengo.png" />
-                                    </div>
-
-                                </div>
-
-                                <div className={styles.area}>
-
-                                    <div className={styles.imagem}>
-                                        <img src="camisaSocialPreta.png" />
-                                    </div>
-
-                                </div>
-
-                                <div className={styles.area}>
-
-                                    <div className={styles.imagem}>
-                                        <img src="armadura.png" />
-                                    </div>
-
-                                </div> */}
-
                             </div>
 
-                            {/* ------------------------------------------------------------------------------------------------ */}
-
-                            {/* <div className={styles.lookEachArea}>
-
-                                <div className={styles.area}>
-
-                                    <div className={styles.imagem}>
-                                        <title>Closet Qualquer Coisa</title>
-                                        <img src="armadura.png" />
-
-                                    </div>
-
-
-
-                                </div>
-
-                                <div className={styles.area}>
-
-                                    <div className={styles.imagem}>
-                                        <img src="armadura.png" />
-                                    </div>
-
-                                </div>
-                                <div className={styles.area}>
-
-                                    <div className={styles.imagem}>
-                                        <img src="armadura.png" />
-                                    </div>
-
-                                </div>
-                            </div> */}
                         </section>
+
+                        <div className={styles.buttonCriaClosetVAzio}>
+                            <button onClick={() => novoClosetVazio()}>clica em mim</button>
+                        </div>
 
                         {/* ------------------------------------------------------------------------------------------------ */}
                     </div>
